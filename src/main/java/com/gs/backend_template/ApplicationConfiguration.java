e370f8f93538af113f337bb458a579a0c78cbb51
Gavin Shaw <gavshaw+github@gmail.com>
2024-08-18 00:06:55 -0400

    Implement logic in H2FooRepository to persist into SQL DB

--- src/main/java/com/gs/backend_template/ApplicationConfiguration.java
+++ src/main/java/com/gs/backend_template/ApplicationConfiguration.java
@@ -3,11 +3,21 @@ package com.gs.backend_template;
 import com.gs.backend_template.database.FooRepository;
 import com.gs.backend_template.database.H2FooRepository;
 import com.gs.backend_template.service.FooService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApplicationConfiguration {
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Value("${spring.datasource.username}")
+    private String dbUser;
+
+    @Value("${spring.datasource.password}")
+    private String dbPass;
 
     @Bean
     public FooService fooService(FooRepository fooRepository) {
@@ -21,6 +31,6 @@ public class ApplicationConfiguration {
 
     @Bean
     public FooRepository fooRepository() {
-        return new H2FooRepository();
+        return new H2FooRepository(dbUrl, dbUser, dbPass);
     }
 }
--- src/main/java/com/gs/backend_template/database/H2FooRepository.java
+++ src/main/java/com/gs/backend_template/database/H2FooRepository.java
@@ -2,24 +2,144 @@ package com.gs.backend_template.database;
 
 import com.gs.backend_template.model.Foo;
 
+import java.sql.*;
+
 public class H2FooRepository implements FooRepository {
+
+    private String dbUrl;
+
+    private String dbUser;
+
+    private String dbPass;
+
+    private final String insertFoo = "insert into foo (desc) values ('%s')";
+
+    private final String updateFoo = "update foo set desc = '%s' where id = %s";
+
+    private final String queryForFoo = "select * from foo where id = %s";
+
+    private final String deleteFoo = "delete from foo where id = %s";
+
+    private final String getLastId = "select max(id) from foo";
+
+    public H2FooRepository(String dbUrl, String dbUser, String dbPass) {
+        this.dbUrl = dbUrl;
+        this.dbUser = dbUser;
+        this.dbPass = dbPass;
+    }
+
     @Override
     public Foo addFoo(Foo foo) {
+        try {
+            Foo resultFoo = null;
+
+            Connection conn = getConnection();
+
+            if (conn != null) {
+                Statement stmt = conn.createStatement();
+                stmt.executeUpdate(String.format(insertFoo, foo.getDesc()));
+
+                ResultSet rs = stmt.executeQuery(getLastId);
+
+                if (rs.next()) {
+                    resultFoo = getFoo(rs.getInt(1));
+                }
+
+                conn.close();
+
+                return resultFoo;
+            }
+        } catch (SQLException e) {
+            return null;
+        }
+
         return null;
     }
 
     @Override
     public Foo deleteFoo(int id) {
+        try {
+            Foo resultFoo = null;
+
+            Connection conn = getConnection();
+
+            if (conn != null) {
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(String.format(deleteFoo, id));
+
+                if (rs.next()) {
+                    resultFoo = Foo.builder().id(rs.getInt(1)).desc(rs.getString(2)).build();
+                }
+
+                conn.close();
+
+                return resultFoo;
+            }
+        } catch (SQLException e) {
+            return null;
+        }
+
         return null;
     }
 
     @Override
     public Foo updateFoo(int id, Foo foo) {
+        try {
+            Foo resultFoo = null;
+
+            Connection conn = getConnection();
+
+            if (conn != null) {
+                Statement stmt = conn.createStatement();
+                stmt.executeUpdate(String.format(updateFoo, foo.getDesc(), id));
+
+                resultFoo = getFoo(id);
+
+                conn.close();
+
+                return resultFoo;
+            }
+        } catch (SQLException e) {
+            return null;
+        }
+
         return null;
     }
 
     @Override
     public Foo getFoo(int id) {
+        try {
+            Foo resultFoo = null;
+
+            Connection conn = getConnection();
+
+            if (conn != null) {
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(String.format(queryForFoo, id));
+
+                if (rs.next()) {
+                    resultFoo = Foo.builder().id(rs.getInt(1)).desc(rs.getString(2)).build();
+                }
+
+                conn.close();
+
+                return resultFoo;
+            }
+        } catch (SQLException e) {
+            return null;
+        }
+
         return null;
+    }
+
+    private Connection getConnection() {
+        try {
+            return DriverManager.getConnection(
+                    dbUrl, dbUser, dbPass
+            );
+        } catch (SQLException e) {
+            return null;
+
+        }
     }
 }
